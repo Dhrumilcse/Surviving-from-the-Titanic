b5b3f54440e799ff40c3897dc5b50d6311f393fa
Dhrumil <dhrumilcse14@gmail.com>
2019-01-04 19:19:48 +0530

    Update feature_1.py

--- feature_1.py
+++ feature_1.py
@@ -8,5 +8,4 @@ all_data = [train_data, test_data]
 passenger_id = test_data['PassengerId']
 
 #Feature Engineering
-#print( train_data[["Pclass","Survived"]].groupby(["Pclass"], as_index = False).mean() )
-print(train_data.dtypes)
+print( train_data[["Pclass","Survived"]].groupby(["Pclass"], as_index = False).mean() )
